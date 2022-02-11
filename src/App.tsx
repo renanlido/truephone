8f021cf27f495a3fcc6113f7d7341e421931a86d
renanlido <renanlido@yahoo.com.br>
2022-02-11 08:41:42 -0300

    refactor: alter export mode to global theme component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from 'styled-components';
 import theme from 'src/global/themes';
-import GlobalStyles from 'src/global/styles/global';
+import GlobalStyles from 'src/global/styles';
 import { Routes } from 'src/routes';
-import { Header } from './components/Header';
+import { Header } from './components/ui';
 // import { Header } from './components/Header';
 
 const App = () => (
