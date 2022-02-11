8839da8a60da067b971044a8c804ef0db00353e2
renanlido <renanlido@yahoo.com.br>
2022-02-11 01:45:15 -0300

    feat: mapping aplication components

---
+++ src/pages/ListValidation/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const ListValidation: React.FC = () => <h1>Ola mundo</h1>;
+
+export { ListValidation };
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const NotFound: React.FC = () => <h1>Ola mundo</h1>;
+
+export { NotFound };
