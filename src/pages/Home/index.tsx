6f8096abc63d55607501b9f9cbcc78f46fe54083
renanlido <renanlido@yahoo.com.br>
2022-02-11 08:19:36 -0300

    chore: alter page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Box } from 'src/components/shapes';
 
-// import { Container } from './styles';
+import { Container, Title } from './styles';
 
-const Home: React.FC = () => <h1>Ola mundo</h1>;
+const Home: React.FC = () => (
+  <Container>
+    <Box>
+      <Title>
+        Verifique a validade de uma lista de mensagens
+        <img
+          alt="👍 thumbs up Emoji on Apple Platform"
+          title="👍 thumbs up Emoji on Apple Platform"
+          data-src="https://emojiguide.com/wp-content/uploads/platform/apple/44140.png"
+          src="https://emojiguide.com/wp-content/uploads/platform/apple/44140.png"
+          data-loaded="true"
+          width={30}
+        />
+      </Title>
+    </Box>
+  </Container>
+);
 
 export { Home };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Container as CustomContainer } from 'src/components/shapes';
+
+export const Container = styled(CustomContainer)``;
+
+export const Title = styled.h1`
+  font-size: 36px;
+  line-height: 42px;
+
+  max-width: 466px;
+
+  text-align: center;
+`;
