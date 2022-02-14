2ec99ef71c4f3d466fe3c7e7151f43b415ad9af1
renanlido <renanlido@yahoo.com.br>
2022-02-14 17:25:19 -0300

    chore: alter database

--- src/shared/services/api/axios.ts
+++ src/shared/services/api/axios.ts
@@ -1,3 +1,8 @@
 import axios from 'axios';
 
-export default axios.create();
+export default axios.create({
+  baseURL: 'https://truephone-api.herokuapp.com/',
+  headers: {
+    'Content-type': 'application/json'
+  }
+});
--- src/shared/services/api/services/truephone.service.ts
+++ src/shared/services/api/services/truephone.service.ts
@@ -2,25 +2,8 @@
 import axios from '../axios';
 import { PostFile } from './types';
 
-// export async function postFile(data: PostFile) {
-//   const response = await axios.post<PostFile>(
-//     'https://truephone-api.herokuapp.com/files/create',
-//     data
-//   );
-
-//   return response.data;
-// }
-
 export async function postFile(data: PostFile) {
-  const response = await axios.post('http://localhost:3333/files/create', {
-    data
-  });
-
-  return response.data;
-}
-
-export async function getFiles() {
-  const response = await axios.post<PostFile>('http://localhost:3333/get/all');
+  const response = await axios.post<PostFile>('/files/create', { data });
 
   return response.data;
 }
